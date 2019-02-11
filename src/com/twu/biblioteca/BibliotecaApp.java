724249d57bd4e2712762c6ff7afff9c5512b806d
Trystan Newbury <trystan.newbury@gmail.com>
2019-02-11 14:29:24 +0000

    Story 1.1 user sees a welcome message works

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,7 +4,10 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-
+        // System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        Printer newPrinter = new Printer("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println(newPrinter.getString());
     }
 }
+
+
---
+++ src/com/twu/biblioteca/Printer.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+public class Printer {
+
+  String printedString;
+
+  public Printer(String stringToBePrinted) {
+
+    printedString = stringToBePrinted;
+
+  }
+
+  public String getString() {
+
+    return printedString;
+
+  }
+
+}
