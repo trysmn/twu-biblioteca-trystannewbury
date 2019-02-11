d625792aebc0c69dabc283333a064340d666e33c
Trystan Newbury <trystan.newbury@gmail.com>
2019-02-11 19:36:00 +0000

    remove test for options from PrinterTests file

--- test/com/twu/biblioteca/PrinterTests.java
+++ test/com/twu/biblioteca/PrinterTests.java
@@ -20,37 +20,4 @@ public class PrinterTests {
 
   }
 
-//  @Test
-//  public void menuOptions() {
-//    String mainMenuTitle;
-//    HashMap<Integer, String> menu;
-////    String optionOne;
-////    String optionTwo;
-////    String optionThree;
-////    String optionFour;
-//
-//    menu = new HashMap<Integer, String>();
-//    mainMenuTitle = "Main menu: Please choose one of the following options by typing its number:";
-//    menu.put(1, "View all books");
-//    menu.put(2, "Checkout a book");
-//    menu.put(3, "Return a book");
-//    menu.put(4, "Quit application");
-////    optionOne = "1. View all books";
-////    optionTwo = "2. Checkout a book";
-////    optionThree = "3. Return a book";
-////    optionFour = "4. Quit application";
-//
-//    Printer printerMenuTitle = new Printer(mainMenuTitle);
-//    Printer printerMenuOptionOne = new Printer(menu.get(1));
-//    Printer printerMenuOptionTwo = new Printer(menu.get(2));
-//    Printer printerMenuOptionThree = new Printer(menu.get(3));
-//    Printer printerMenuOptionFour = new Printer(menu.get(4));
-//
-//    assertEquals("Main menu: Please choose one of the following options by typing its number:", printerMenuTitle.menuOptions());
-//    assertEquals("View all books", printerMenuOptionOne.menuOptions());
-//    assertEquals("Checkout a book", printerMenuOptionTwo.menuOptions());
-//    assertEquals("Return a book", printerMenuOptionThree.menuOptions());
-//    assertEquals("Quit application", printerMenuOptionFour.menuOptions());
-//
-//  }
 }
