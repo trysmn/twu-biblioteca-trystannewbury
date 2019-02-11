3468a265b57743598620298822fa32c46601a86c
Trystan Newbury <trystan.newbury@gmail.com>
2019-02-11 20:14:50 +0000

    add instructions directory with Release 1 requirements and introduction to Biblioteca as files

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
@@ -7,7 +9,16 @@ public class BibliotecaApp {
         Printer newPrinter = new Printer("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
         System.out.println(newPrinter.shouldWelcome());
 
+        HashMap<Integer, String> hash;
+        hash = new HashMap<Integer, String>();
 
+        hash.put(1, "View all books");
+        hash.put(2, "Checkout a book");
+        hash.put(3, "Return a book");
+        hash.put(4, "Quit application");
+        Menu newMenu = new Menu(hash);
+
+        System.out.println(newMenu.showAllOptions());
 
     }
 }
