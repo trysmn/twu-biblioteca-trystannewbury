320aa373df7d6535d59f27ddd1c27f57a1e4f525
Trystan Newbury <trystan.newbury@gmail.com>
2019-02-12 07:59:40 +0000

    remove unnecessary method from Printer class

--- src/com/twu/biblioteca/Printer.java
+++ src/com/twu/biblioteca/Printer.java
@@ -16,19 +16,4 @@ public class Printer {
 
   }
 
-//  public HashMap<Integer, String> menuOptions() {
-//
-//    HashMap<Integer, String> menu;
-//
-//    menu = new HashMap<Integer, String>();
-//
-//    menu.put(1, "View all books");
-//    menu.put(2, "Checkout a book");
-//    menu.put(3, "Return a book");
-//    menu.put(4, "Quit application");
-//
-//    return menu;
-//
-//  }
-
 }
